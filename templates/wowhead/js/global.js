9e8ab2e368736aba0c86df8a8e891d018ac5cabe
robinxu <29698868@qq.com>
2019-02-28 17:12:35 +0800

    rollback

--- templates/wowhead/js/global.js
+++ templates/wowhead/js/global.js
@@ -837,7 +837,7 @@ function g_initHeaderMenus() {
 			d = d.substr(0, j)
 		}
 		//c.menu = [[0, "Deutsch", (g_locale.id != 3 ? d.replace(g, "de") : null)], [0, "English", (g_locale.id != 0 ? d.replace(g, "www") : null)], [0, "Espa" + String.fromCharCode(241) + "ol", (g_locale.id != 6 ? d.replace(g, "es") : null)], [0, "Fran" + String.fromCharCode(231) + "ais", (g_locale.id != 2 ? d.replace(g, "fr") : null)], [0, String.fromCharCode(1056, 1091, 1089, 1089, 1082, 1080, 1081), (g_locale.id != 7 ? d.replace(g, "ru") : null)]];
-		c.menu = [[0, "English", (g_locale.id != 0 ? "?locale=0" : null)], [0, "??", (g_locale.id != 4 ? "?locale=4" : null)]];
+		c.menu = [[0, "English", (g_locale.id != 0 ? "?locale=0" : null)], [0, "中文", (g_locale.id != 4 ? "?locale=4" : null)]];
 		c.menu.rightAligned = 1;
 		if (g_locale.id != 25) {
 			c.menu[{
